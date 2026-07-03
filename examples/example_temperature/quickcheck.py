9a54efb729c620e48a4a6e0560c1b51cf6e541ca
Ana <ana.stuhec44@gmail.com>
2026-07-03 08:06:18 +0200

    quick check of results plotting

--- examples/example_temperature/quickcheck.py
+++ examples/example_temperature/quickcheck.py
@@ -1,41 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
-results = np.load('output.npz', allow_pickle=True)
+def main(Vc, tperp):
+    suffix = f'Vc{Vc}_tperp{tperp}'
+    results = np.load(f'output/output_{suffix}.npz')
 
-L12 = results['L12']
-L12q = results['L12q']
-L11 = results['L11']
+    T = results['Ts']
+    mu = results['mus']
 
+    '''    L12_boltz = results['L12_boltz']
+    L11_boltz = results['L11_boltz']
 
-T = results['Ts']
+    L12 = results['L12']
+    L12q = results['L12q']
+    L11 = results['L11']
 
-L12_boltz = results['L12_boltz']
-L11_boltz = results['L11_boltz']
-S_boltz = -L12_boltz/L11_boltz/T
-S = -L12/L11/T
-Sq = -L12q/L11/T
+    L12_0 = results['L12_0']
+    L12q_0 = results['L12q_0']
+    L11_0 = results['L11_0']
+    L12_corr = results['L12_corr']
+    L12q_corr = results['L12q_corr']
+    L11_corr = results['L11_corr']
 
-L12_0 = results['L12_0']
-L12q_0 = results['L12q_0']
-L11_0 = results['L11_0']
-L12_corr = results['L12_corr']
-L12q_corr = results['L12q_corr']
-L11_corr = results['L11_corr']
+    S_boltz = -L12_boltz/L11_boltz/T
+    S = -L12/L11/T
+    Sq = -L12q/L11/T
+    S0 = -L12_0/L11_0/T
+    Sq0 = -L12q_0/L11_0/T
+    S_corr = -L12_corr/L11_corr/T
+    Sq_corr = -L12q_corr/L11_corr/T
 
-S0 = -L12_0/L11_0/T
-Sq0 = -L12q_0/L11_0/T
-S_corr = -L12_corr/L11_corr/T
-Sq_corr = -L12q_corr/L11_corr/T
+    fig, ax = plt.subplots(ncols=2,figsize=(8,4))
 
+    ax[1].plot(T, S, color='red')
+    ax[1].plot(T, S0, color='maroon', ls='dashed', lw=2)
 
-#plt.plot(T, S_boltz)
-plt.plot(T, S, color='red')
-plt.plot(T, S0, ls='dashed', color='black')
-plt.plot(T, S + Sq, color='blue')
-plt.plot(T, S0 + Sq0, ls='dashed', color='black')
-plt.plot(T, S_corr + Sq_corr, ls='dotted', color='green')
+    ax[1].plot(T, S+Sq, color='blue')
+    ax[1].plot(T, S0+Sq0, color='navy', ls='dashed', lw=2)
 
-plt.ylim(-3,3)
-plt.xlim(0,0.1)
-plt.show()
+    ax[1].plot(T, S_corr+Sq_corr, color='magenta', lw=2)'''
+    plt.plot(T, mu)
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="plot results Seebeck(temperature)")
+    parser.add_argument('Vc', help='Vc')
+    parser.add_argument('tperp', help='tperp')
+    args = parser.parse_args()
+    main(args.Vc, args.tperp)
