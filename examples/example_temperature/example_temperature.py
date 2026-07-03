cce4634d60f190bc8323be37fdd7cae570862b3b
Ana <ana.stuhec44@gmail.com>
2026-07-03 08:05:01 +0200

    script

--- examples/example_temperature/example_temperature.py
+++ examples/example_temperature/example_temperature.py
@@ -14,16 +14,16 @@ DIR = 'C:\\Users\\anast\\OneDrive\\Namizje\\takarada-ana\\'
 sys.path.insert(0, DIR + 'main-programs')
 import takarada_module as module
 
-def main(T_start = 0.02, T_end = 0.06):
+def main(T_start = 0.03, T_end = 0.06):
         # Parse command line: input_file required, output_file optional
     parser = argparse.ArgumentParser(description="Run takarada module with input and optional output.")
     parser.add_argument('suffix', help='suffix for input and output files')
-
+    
     args = parser.parse_args()
     suffix = args.suffix
 
-    input_file_path = os.path.join(DIR + f'examples\\example_temperature\\input_{suffix}.json')
-    output_file_path = os.path.join(DIR + f'examples\\example_temperature\\output\\output_{suffix}.npz')
+    input_file_path = os.path.join(DIR + f'examples/example_temperature/input_{suffix}.json')
+    output_file_path = os.path.join(DIR + f'examples/example_temperature/output/output_{suffix}.npz')
 
     ## (1) Initialize model
     s = module.model(input_file_path)
@@ -38,7 +38,7 @@ def main(T_start = 0.02, T_end = 0.06):
     ## (4) Save results
     evaluate_transport_DC = s.config['evaluate_transport_DC']
     evaluate_vertex_DC = s.config['evaluate_vertex_DC']
-    data = s.collect_data(evaluate_transport_DC=evaluate_transport_DC, evaluate_vertex_DC=evaluate_vertex_DC, merge=True)
+    data = s.collect_data(evaluate_transport_DC=evaluate_transport_DC, evaluate_vertex_DC=evaluate_vertex_DC, merge=False)
     np.savez(output_file_path, **data)
 
 if __name__ == "__main__":
